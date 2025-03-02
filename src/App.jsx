0668fca247edbb2346fd182741cda069cab1aec0
Jacob Marsh <jake.marsh@live.com.au>
2025-03-01 20:51:46 -0500

    Restart project

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,15 @@
-import React from "react";
-import { GetData } from "./components/UtilsScripts/GetData.jsx";
-import GetProps from "./components/UtilsScripts/GetProps.jsx";
-import Controls from "./components/Controls.jsx";
-import Legend from "./components/Legend.jsx";
-import Chart from "./components/Chart.jsx";
-import "./App.css";
-import Description from "./components/Description.jsx";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-  // const csvUrl = "/processed_sweepdata.csv"; // Old data pre-mirror
-  const csvUrl = `${import.meta.env.BASE_URL}mirrored_data.csv`; // Mirrored so that negative distances shown too
-  const data = GetData(csvUrl) || [];
-  const {
-    controlsProps,
-    legendProps,
-    chartAxesProps,
-    chartMarksProps,
-    chartLineProps,
-  } = GetProps(data);
-  if (!data || data.length === 0) {
-    return <div>Loading or no data found...</div>;
-  }
+function App() {
 
   return (
-    <div className="h-screen overflow-auto p-10">      
-      <div className="flex items-center [@media(min-height:955px)]:h-full justify-center flex-row max-[1340px]:flex-col">
-        <div className="Figure">
-          <Legend {...legendProps} />
-          <Chart axesProps={chartAxesProps} marksProps={chartMarksProps} lineProps={chartLineProps} />
-          <Controls {...controlsProps} />
-        </div>
-        {/* When the screen is less than 1340px, move Description 100px to the right */}
-        <div className="max-[1340px]:ml-[95px]">
-          <Description />
-        </div>
-      </div>
-    </div>
-  );
-};
+  <div className="flex items-center justify-center h-screen bg-gray-100">
+    <h1 className="text-3xl font-bold text-blue-500">Hello Tailwind!</h1>
+  </div>
+  )
+}
 
-export default App;
+export default App
