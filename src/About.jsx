e74672c28d58f6ec1a9d11dbb48992d02989273d
Jacob Marsh <jake.marsh@live.com.au>
2025-03-16 22:54:38 -0400

    Added images to about page

--- src/About.jsx
+++ src/About.jsx
@@ -13,10 +13,7 @@ const About = () => {
     className="absolute top-[54px] left-0 w-1/3 opacity-45 filter grayscale contrast-125 brightness-75 invert-[18%] z-[-1] ml-3.5"
   />
   {/* Right Image (Mirrored, Always 70px from the top) */}
-  <img 
-    src="/local_images/out_minimini.png"
-    className="absolute top-[54px] right-0 scale-x-[-1] w-1/3 opacity-45 filter grayscale contrast-125 brightness-75 invert-[18%] z-[-1] mr-3.5"
-  />
+
 
   {/* Top Border Section */}
   <div className="relative flex items-center w-full h-[120px]">
@@ -33,7 +30,7 @@ const About = () => {
   <div className="flex-grow flex flex-col items-center justify-center text-[#3e3c3]">
     <h1 className="text-7xl font-bold Hero ml-9 mb-15">Jacob I Marsh</h1>
     <h3 className="text-4xl font-bold Hero ml-9 mt-15">
-      Scroll down for a showcase of my bioinformatics/popgen research!
+      About me...
     </h3>
   </div>
 
@@ -46,6 +43,27 @@ const About = () => {
     />
     <div className="flex-1 border-t-2 border-[#3e3c38] border-dotted mr-7"></div>
   </div>
+<div className="grid grid-cols-2 grid-rows-3 gap-4 w-114 absolute top-[64px] right-0 opacity-90 z-[-1] mr-6.5"> {/* Use gap-1 for 4px spacing */}
+  {/* Top Left (First Image) */}
+  <div className="w-55 h-70 overflow-hidden">
+    <img src="/local_images/PicAus.jpeg" className="w-full h-auto object-cover object-top" alt="Aus pic"/>
+  </div>
+
+  {/* Top Right (Second Image) */}
+  <div className="w-55 h-70 overflow-hidden">
+    <img src="/local_images/PicCharleston.jpeg" className="w-full h-full object-cover object-center" alt="Charleston pic"/>
+  </div>
+
+  {/* Bottom Left (Empty Space) */}
+
+  <div className="whitespace-pre-line justify-center text-center">Southwest Australia</div>
+  {/* Bottom Right (Third Image) */}
+  <div className="w-55 h-70 overflow-hidden">
+    <img src="/local_images/PicCH.jpeg" className="w-full h-auto object-cover object-top" alt="CH pic"/>
+  </div>
+  <div></div>
+  <div className="whitespace-pre-line justify-center text-center">North Carolina, USA</div>
+</div>
 </div>
 
 <div className="fixed top-4 right-4 flex flex-col z-50">
@@ -64,6 +82,9 @@ const About = () => {
     <img src="/local_images/github_icon.svg" className="w-8 h-8 opacity-50 hover:opacity-100 m-2" alt="GitHub" />
   </a>
 </div>
+
+
+
 
       <p className="flex flex-col items-center justify-center text-center whitespace-pre-line pb-4 mt-[-5px]">
   Made by me!
